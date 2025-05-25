59443aac2da66b6d2357dba4b55bb4959396beeb
Sushabhan <sushabhanmajumder878@gmail.com>
2025-05-25 05:37:37 +0530

    compleated the Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import styles from "./styles";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="bg-primary overflow-hidden w-full">
+      {/* Navbar */}
+      <div className={`${styles.paddingX} ${styles.flexCenter} `}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+      {/* Hero */}
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>Hero</div>
+      </div>
+      {/* Other */}
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats, Business, Bulling, testimonial, Click to action, Footer
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
